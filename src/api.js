402b42f0818515b7b3df6044249374f44a54d346
Krzysztof-GoIT <145707752+Krzysztof-GoIT@users.noreply.github.com>
2024-03-03 16:38:03 +0100

    HW Pull

---
+++ src/api.js
@@ -0,0 +1,15 @@
+/* Temporary User & Pass: wadaf99514@aersm.com
+Your API key: 42677671-5bcb3e424f2e87a0f8bfdddc9 */
+
+export const BASE_URL = 'https://pixabay.com/api/';
+export const options = {
+  params: {
+    key: '42677671-5bcb3e424f2e87a0f8bfdddc9',
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+  },
+};
